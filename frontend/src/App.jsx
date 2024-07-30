32eb025ca5010796767971749578425c96319292
KAVIRAJec <kaviraj.ec21@bitsathy.ac.in>
2024-07-30 10:36:11 +0530

    included background image[ADD]

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -47,37 +47,43 @@ const App = () => {
 
 
   return (
-    <div>
+    <div className="min-h-screen bg-slate-200 bg-cover bg-top">
+    <div className="absolute inset-0 bg-[url('./assets/background.png')] bg-cover opacity-80"></div>
       {Object.keys(sensorData).length === 0 ? (
-        <div className="flex justify-center items-center">
-          <Card className="w-[350px]">
-            <CardHeader>
-              <CardTitle>Something Went Wrong</CardTitle>
-              <CardDescription>404 Error..</CardDescription>
-            </CardHeader>
-            <CardContent>
-              No sensor data available.
-            </CardContent>
+        <div className="flex justify-center items-center h-screen align-middle">
+          <Card className="relative block max-w-sm rounded-lg bg-slate-200 p-6 shadow-lg">
+            <div className="absolute inset-0 bg-[url('./assets/cardBg.png')] bg-cover opacity-40"></div>
+            <div className="relative z-10">
+              <CardHeader>
+                <CardTitle className='text-center'>Something Went Wrong</CardTitle>
+                <CardDescription className='text-center'>404 Error..</CardDescription>
+              </CardHeader>
+              <CardContent className='flex justify-center text-center text-3xl font-bold'>
+                No sensor data available.
+              </CardContent>
+            </div>
           </Card>
         </div>
       ) : (
         <Card className='flex m-8 border-slate-700 rounded-3xl h-auto'>
-        {Object.keys(sensorData).map(device_id => (
-          <Card className="w-[350px] m-4 border-slate-400">
-            <CardHeader>
-              <CardTitle>Device: {sensorData[device_id].device}</CardTitle>
-              <CardDescription className='font-semibold'>Description: {sensorData[device_id].description}</CardDescription>
-            </CardHeader>
-            <CardContent>
-              <Label className='block text-xl'>{sensorData[device_id].TName}:
-              <span className="text-2xl flex justify-center items-center font-bold"> {sensorData[device_id].temp} {sensorData[device_id].TUnit}</span>
-              </Label>
-              <Label className='block text-xl'>{sensorData[device_id].HName}:
-              <span className="text-2xl flex justify-center items-center  font-bold">{sensorData[device_id].humidity} {sensorData[device_id].HUnit}</span>
-              </Label>
-            </CardContent>
-          </Card>
-        ))}
+          {Object.keys(sensorData).map(device_id => (
+            <Card className="relative block w-[350px] rounded-lg bg-slate-200 p-6 shadow-lg m-4">
+            <div className="absolute inset-0 bg-[url('./assets/cardBg.png')] bg-cover opacity-40"></div>
+            <div className="relative z-10"></div>
+              <CardHeader>
+                <CardTitle>Device: {sensorData[device_id].device}</CardTitle>
+                <CardDescription className='font-semibold'>Description: {sensorData[device_id].description}</CardDescription>
+              </CardHeader>
+              <CardContent>
+                <Label className='block text-xl'>{sensorData[device_id].TName}:
+                  <span className="text-2xl flex justify-center items-center font-bold"> {sensorData[device_id].temp} {sensorData[device_id].TUnit}</span>
+                </Label>
+                <Label className='block text-xl'>{sensorData[device_id].HName}:
+                  <span className="text-2xl flex justify-center items-center  font-bold">{sensorData[device_id].humidity} {sensorData[device_id].HUnit}</span>
+                </Label>
+              </CardContent>
+            </Card>
+          ))}
         </Card>
       )}
     </div>
