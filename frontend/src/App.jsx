fcd545f6da6b4fcc56798c9e86b026556c15e7a8
KAVIRAJec <kaviraj.ec21@bitsathy.ac.in>
2024-07-30 15:04:59 +0530

    Background image[FIX]

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -24,7 +24,7 @@ const App = () => {
               ...prevData,
               [readData.device_id]: {
                 device: readData.device_id,
-                description: `This is ${readData.device_id}`,
+                description: readData && readData.device_id === 1 ? "Indoor Unit" : "Outdoor Unit",
                 TName: "Temperature",
                 temp: readData.temp,
                 TUnit: "Celsius",
@@ -48,39 +48,38 @@ const App = () => {
 
   return (
     <div className="min-h-screen bg-slate-200 bg-cover bg-top">
-    <div className="absolute inset-0 bg-[url('./assets/background.png')] bg-cover opacity-80"></div>
+      <div className="absolute inset-0 bg-[url('./assets/background.png')] bg-cover opacity-80"></div>
       {Object.keys(sensorData).length === 0 ? (
         <div className="flex justify-center items-center h-screen align-middle">
           <Card className="relative block max-w-sm rounded-lg bg-slate-200 p-6 shadow-lg">
-            <div className="absolute inset-0 bg-[url('./assets/cardBg.png')] bg-cover opacity-40"></div>
-            <div className="relative z-10">
-              <CardHeader>
-                <CardTitle className='text-center'>Something Went Wrong</CardTitle>
-                <CardDescription className='text-center'>404 Error..</CardDescription>
-              </CardHeader>
-              <CardContent className='flex justify-center text-center text-3xl font-bold'>
-                No sensor data available.
-              </CardContent>
-            </div>
+            <CardHeader>
+              <CardTitle className='text-center'>Something Went Wrong</CardTitle>
+              <CardDescription className='text-center'>404 Error..</CardDescription>
+            </CardHeader>
+            <CardContent className='flex justify-center text-center text-3xl font-bold'>
+              No sensor data available.
+            </CardContent>
           </Card>
         </div>
       ) : (
         <Card className='flex m-8 border-slate-700 rounded-3xl h-auto'>
           {Object.keys(sensorData).map(device_id => (
-            <Card className="relative block w-[350px] rounded-lg bg-slate-200 p-6 shadow-lg m-4">
-            <div className="absolute inset-0 bg-[url('./assets/cardBg.png')] bg-cover opacity-40"></div>
-            <div className="relative z-10"></div>
+            <Card className="relative block w-[350px] rounded-lg bg-slate-200 p-6 shadow-lg m-7">
               <CardHeader>
                 <CardTitle>Device: {sensorData[device_id].device}</CardTitle>
-                <CardDescription className='font-semibold'>Description: {sensorData[device_id].description}</CardDescription>
+                <CardDescription className='font-semibold'>{sensorData[device_id].description}</CardDescription>
               </CardHeader>
               <CardContent>
-                <Label className='block text-xl'>{sensorData[device_id].TName}:
-                  <span className="text-2xl flex justify-center items-center font-bold"> {sensorData[device_id].temp} {sensorData[device_id].TUnit}</span>
-                </Label>
-                <Label className='block text-xl'>{sensorData[device_id].HName}:
-                  <span className="text-2xl flex justify-center items-center  font-bold">{sensorData[device_id].humidity} {sensorData[device_id].HUnit}</span>
-                </Label>
+                <div className="px-10 py-3 rounded-2xl bg-gray-300 mb-4">
+                  <Label className='block text-xl'>{sensorData[device_id].TName}:
+                    <span className="text-2xl flex justify-center items-center font-bold"> {sensorData[device_id].temp} {sensorData[device_id].TUnit}</span>
+                  </Label>
+                </div>
+                <div className="px-10 py-3 rounded-2xl bg-gray-300">
+                  <Label className='block text-xl'>{sensorData[device_id].HName}:
+                    <span className="text-2xl flex justify-center items-center  font-bold">{sensorData[device_id].humidity} {sensorData[device_id].HUnit}</span>
+                  </Label>
+                </div>
               </CardContent>
             </Card>
           ))}
