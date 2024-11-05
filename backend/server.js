a0c0109f7e6774832be3dbd73a2b6a4ce8f004f8
KAVIRAJec <kaviraj.ec21@bitsathy.ac.in>
2024-11-05 14:03:37 +0530

    store incoming data in xls[ADD]

--- backend/server.js
+++ backend/server.js
@@ -1,23 +1,44 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const cors = require('cors');
+const xlsx = require('xlsx');
+const fs = require('fs');
 
+const cors = require('cors');
 const app = express();
 app.use(cors());
+
 const port = 3000;
 
 app.use(bodyParser.json());
 
-let sensorData = {};
-
 app.get('/sensor', (req, res) => {
     const { device_id, temp, humidity } = req.query;
 
-    sensorData = {
+    const sensorData = {
         device_id: device_id,
         temp: temp,
         humidity: humidity
     };
+
+    let workbook;
+    let worksheet;
+    const filePath = 'sensorData.xlsx';
+
+    if (fs.existsSync(filePath)) {
+        workbook = xlsx.readFile(filePath);
+        worksheet = workbook.Sheets[workbook.SheetNames[0]];
+    } else {
+        workbook = xlsx.utils.book_new();
+        worksheet = xlsx.utils.json_to_sheet([]);
+        xlsx.utils.book_append_sheet(workbook, worksheet, 'Sheet1');
+    }
+
+    const data = xlsx.utils.sheet_to_json(worksheet);
+    data.push(sensorData);
+    const newWorksheet = xlsx.utils.json_to_sheet(data);
+    workbook.Sheets[workbook.SheetNames[0]] = newWorksheet;
+
+    xlsx.writeFile(workbook, filePath);
 
     res.send({
         message: 'Data received successfully',
@@ -26,12 +47,24 @@ app.get('/sensor', (req, res) => {
 });
 
 app.post('/sensor', (req, res) => {
+    const filePath = 'sensorData.xlsx';
+
+    if (!fs.existsSync(filePath)) {
+        return res.status(404).send({ message: 'No file found' });
+    }
+
+    const workbook = xlsx.readFile(filePath);
+    const worksheet = workbook.Sheets[workbook.SheetNames[0]];
+    const data = xlsx.utils.sheet_to_json(worksheet);
+
+    if (data.length === 0) {
+        return res.status(404).send({ message: 'No data found' });
+    }
+    const recentData = data[data.length - 1];
     res.send({
-        message: 'Data get successfully',
-        device_id: sensorData.device_id,
-        temp: sensorData.temp,
-        humidity: sensorData.humidity
-});
+        message: 'Data retrieved successfully',
+        ...recentData
+    });
 });
 
 app.listen(port, () => {
