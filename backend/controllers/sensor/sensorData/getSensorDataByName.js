88a7eda77f54fcd71e8750f1322ecd03d0cea90a
PRIYANKAec <priyankaa261103@gmail.com>
2025-02-28 13:48:21 +0530

    Implemented GetSensorData By Sensor and Project Name in Backend[PRIYA-ADD]

---
+++ backend/controllers/sensor/sensorData/getSensorDataByName.js
@@ -0,0 +1,40 @@
+const SensorModel = require("../../../models/sensorModel");
+const ProjectModel = require("../../../models/projectModel");
+const { formatResponse } = require("../../../utils/helper");
+const Joi = require('joi');
+
+const getSensorDataByName = async (req, res) => {
+    const paramsSchema = Joi.object({
+        projectName: Joi.string().required(),
+        sensorName: Joi.string().required(),
+    });
+
+    const { error: paramsError, value: paramsValue } = paramsSchema.validate(req.params);
+    if (paramsError) {
+        const formattedError = paramsError.details.map(detail => detail.message.replace(/"/g, '')).join(', ');
+        return res.status(400).json(formatResponse('error', 'Validation Error', formattedError));
+    }
+    
+    try {
+        // Check if project exists
+        const projectExists = await ProjectModel.findProjectByName(paramsValue.projectName);
+        if (!projectExists) {
+            return res.status(404).json(formatResponse('error', 'Project not found'));
+        }
+
+        // Check if Sensor exists
+        const sensor = await SensorModel.findSensorByName(paramsValue.sensorName);
+        if (!sensor) {
+            return res.status(404).json(formatResponse('error', 'Sensor not found'));
+        }
+
+        const sensorDataByName = await SensorModel.findSensorByNameAndProject(paramsValue.projectName, paramsValue.sensorName);
+
+        req.io.emit('sensorData', sensorDataByName);
+        res.status(201).json(formatResponse('success', 'Sensor data received successfully', sensorDataByName));
+    } catch (error) {
+        res.status(500).json(formatResponse('error', 'Internal Server Error', error.message));
+    }
+};
+
+module.exports = getSensorDataByName;
