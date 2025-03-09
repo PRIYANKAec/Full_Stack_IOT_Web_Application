7f8074e7b297ef7435e625663755f0a8fe1d45c3
PRIYANKAec <priyankaa261103@gmail.com>
2025-03-09 12:52:46 +0530

    Added SampleCode for MicroControllersin TutorialPage[PRIYA-ADD]

--- frontend/src/pages/Student/Tutorial.jsx
+++ frontend/src/pages/Student/Tutorial.jsx
@@ -10,6 +10,7 @@ import { motion } from "framer-motion";
 import { esp_code, python_code } from "@/assets/code";
 import { useAuth } from "@/context/AuthContext";
 import { getToken } from "@/utils/auth";
+import { Sample_ESP32_Code, Sample_RaspberryPi_Code } from "@/assets/exampleCode";
 
 const Tutorial = () => {
   const { user } = useAuth()
@@ -142,6 +143,62 @@ const Tutorial = () => {
               </TabsContent>
             </Tabs>
           </section>
+
+          <section className="mb-8">
+            <h2 className="text-lg tex font-bold mb-2">
+              Example Arduino Code for IoT-based Project
+            </h2>
+            <Tabs defaultValue="esp32" className="flex flex-col items-center">
+              <TabsList className="mb-4 flex justify-center w-[240px]">
+                <TabsTrigger value="esp32">ESP32/ESP8266</TabsTrigger>
+                <TabsTrigger value="raspberry-pi">Raspberry Pi</TabsTrigger>
+              </TabsList>
+
+              <TabsContent value="esp32">
+                <h2 className="text-lg font-semibold mb-2">
+                  ESP32/ESP8266 Code
+                </h2>
+                <pre className="bg-gray-100 p-4 rounded-lg overflow-auto">
+                  <code className="whitespace-pre-wrap break-all">
+                  {Sample_ESP32_Code}
+                  </code>
+                </pre>
+              </TabsContent>
+
+              <TabsContent value="raspberry-pi">
+                <h2 className="text-lg font-semibold mb-2">
+                  Raspberry Pi Code
+                </h2>
+                <pre className="bg-gray-100 p-4 rounded-lg overflow-auto">
+                  <code className="whitespace-pre-wrap break-all">
+                  {Sample_RaspberryPi_Code}
+                  </code>
+                </pre>
+              </TabsContent>
+            </Tabs>
+
+            <section className="mb-8">
+            <h2 className="text-lg font-bold mb-2">Work Flow of the Code</h2>
+            <ul className="list-disc list-inside mb-4">
+              <li>Begins Serial Communication at 115200 baud rate.</li>
+              <li>Initializes Ultrasonic Sensor (sets TRIG_PIN as output and ECHO_PIN as input).</li>
+              <li>Initializes DHT11 Sensor and retrieves minimum delay between readings.</li>
+              <li>Configures LED and Switch (LED_PIN is set to LOW initially).</li>
+              <li>Connects to Wi-Fi using connectToWiFi().</li>
+            </ul>
+            </section>
+          </section>
+          <section className="mb-8">
+            <h2 className="text-lg font-bold mb-2">How it Works</h2>
+            <ul className="list-disc list-inside mb-4">
+              <li>Sends a 10µs pulse to TRIG_PIN.</li>
+                <li>Measures the time duration of the pulse reflected back to ECHO_PIN.</li>
+                <li>Uses the formula:
+                Distance (cm) = Time (µs) ×  0.034 / 2</li>
+                <li>Prints the distance to the Serial Monitor.</li>
+            </ul>
+          </section>
+
         </CardContent>
       </Card>
     </motion.div>
