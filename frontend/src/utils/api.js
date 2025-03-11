b7da9a2ddf75198b416813ff516b99a8f6a3edcc
KAVIRAJec <kaviraj.ec21@bitsathy.ac.in>
2025-03-11 20:02:12 +0530

    workflow backend env passing[KAVI-ADD]

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -2,7 +2,6 @@ import axios from 'axios';
 import { getAuthHeader } from './auth';
 
 const API_URL = import.meta.env.VITE_APP_BACKEND_URL;
-console.log("API_URL",API_URL)
 
 const api = axios.create({
   baseURL: API_URL,
