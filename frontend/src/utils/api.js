e73b6e0d8d935195b41260968a9206db6dc55da8
KAVIRAJec <kaviraj.ec21@bitsathy.ac.in>
2025-03-11 18:51:40 +0530

    .env passage in frontend[KAVI-TEST]

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -2,6 +2,7 @@ import axios from 'axios';
 import { getAuthHeader } from './auth';
 
 const API_URL = import.meta.env.VITE_APP_BACKEND_URL;
+console.log("API_URL",API_URL)
 
 const api = axios.create({
   baseURL: API_URL,
